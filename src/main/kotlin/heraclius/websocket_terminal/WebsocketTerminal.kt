6d8d56a8c23444d63c136e459871a510a4da08d3
saber71 <1533357473@qq.com>
2024-11-03 09:19:19 +0800

    fix(websocket-terminal): 优化 WebSocket 会话管理
    - 重命名 create 方法为 instance，以更准确地反映其作为工厂方法的角色
    - 在 write 方法中添加异常捕获，处理 ClosedSendChannelException
    - 当发送通道关闭时，从实例缓存中移除对应的 WebsocketTerminal 实例

--- src/main/kotlin/heraclius/websocket_terminal/WebsocketTerminal.kt
+++ src/main/kotlin/heraclius/websocket_terminal/WebsocketTerminal.kt
@@ -5,18 +5,24 @@ import io.ktor.websocket.Frame
 import io.ktor.websocket.WebSocketSession
 import io.ktor.websocket.readText
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import kotlinx.coroutines.channels.ClosedSendChannelException
 
 class WebsocketTerminal(val wsSession: WebSocketSession) : TerminalAdapter {
     companion object {
         private val instCache = mutableMapOf<WebSocketSession, WebsocketTerminal>()
 
-        fun create(wsSession: WebSocketSession): WebsocketTerminal {
+        fun instance(wsSession: WebSocketSession): WebsocketTerminal {
             return instCache.getOrPut(wsSession) { WebsocketTerminal(wsSession) }
         }
     }
 
     override suspend fun write(str: String) {
-        wsSession.send(Frame.Text(str))
+        try {
+            wsSession.send(Frame.Text(str))
+        } catch (e: ClosedSendChannelException) {
+            instCache.remove(wsSession)
+            throw e
+        }
     }
 
     override suspend fun input(): String {
